3f16be13f50059fa9a8e19316f15da58f6ae37b2
Quip13 <random.emailforcrap@gmail.com>
2015-04-20 04:18:25 -0400

    Revert "Revert "small changes to visualization""
    This reverts commit 90af8f4cb3eb4949b40ce428b86242623d04cdcc.

--- src/findEdges.m
+++ src/findEdges.m
@@ -6,10 +6,11 @@ if nargin == 1
    highPassFactor = 2;
    boost = 0.3;
    filter = fspecial('gaussian', 30, 10);
-   thresh = 0.0055;
+   thresh = 0.005;
    sigma = 10;
    edgeType = 'Roberts';
 end
+I = imfilter(I, fspecial('gaussian', 25, 15));
 if size(I,3) == 3
     grayIm = rgb2gray(I);
 else

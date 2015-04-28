09c8ec0ea5c38da4c843898cf2a4b8e16f38fd85
Kyle Xiong <kxiong1@jhu.edu>
2015-04-27 23:48:19 -0400

    Modified trainSVM file and svmTrainer driver to take in 3 labeled images at a time and use the histogram feature.

---
+++ drivers/svmTrainer.m
@@ -0,0 +1,73 @@
+tic;
+%File name constants
+%Make sure to change directory to cis-ii-sinus-surgery sandbox directory
+close all;
+imageHeader = 'rec-000098';
+imageType = '.bmp';
+labeledImageType = '.png';
+outputHeader = fullfile('output');
+inputHeader = fullfile('input');
+I1 = imread(fullfile('input', strcat(imageHeader, '20', imageType)));
+I2 = imread(fullfile('input', strcat(imageHeader, '47', imageType)));
+I3 = imread(fullfile('input', strcat(imageHeader, '69', imageType)));
+I0 = imread(fullfile('input', strcat(imageHeader, '35', imageType)));
+labeled1 = imread(fullfile('input', strcat('labeled-', imageHeader, '20', labeledImageType)));
+labeled2 = imread(fullfile('input', strcat('labeled-', imageHeader, '47', labeledImageType)));
+labeled3 = imread(fullfile('input', strcat('labeled-', imageHeader, '69', labeledImageType)));
+
+% Train SVM on hand labeled image
+disp('Training on hand labeled image');
+trainingSetPoints1 = extractLearningSet(labeled1);
+trainingSetPoints2 = extractLearningSet(labeled2);
+trainingSetPoints3 = extractLearningSet(labeled3);
+[svm, trainingFeatures, trainingLabels] = trainSVM(I1, I2, I3, trainingSetPoints1, trainingSetPoints2, trainingSetPoints3);
+
+% Evaluate using a separate image
+disp('Evaluating using separate image');
+[features, edges] = extractFeaturesHistBox(I0);
+predictedLabels = predict(svm, features);
+
+% Find features labeled with 1 (occluding contour)
+disp('Finding features labeled with 1');
+edgeCoords = find(edges == 1);
+featureEdgeCoords = find(predictedLabels == 1);
+featureEdges = edgeCoords(featureEdgeCoords);
+testFeatures = zeros(size(edges));
+testFeatures(featureEdges) = 1;
+
+
+% Find features labeled with 0 (not an occluding contour)
+disp('Finding features labeled with 0');
+nonFeatureEdgeCoords = find(predictedLabels == 0);
+nonFeatureEdges = edgeCoords(nonFeatureEdgeCoords);
+testNonFeatures = zeros(size(edges));
+testNonFeatures(nonFeatureEdges) = 1;
+figure;
+imshow([testFeatures, testNonFeatures]);
+title('Test Features vs. NonFeatures');
+
+% Visualize support vectors
+% sv = svm.SupportVectors;
+% figure
+% gscatter(trainingFeatures(:,1), trainingFeatures(:,2),trainingLabels)
+% hold on
+% plot(sv(:,1), sv(:,2),'ko','MarkerSize',10)
+% legend('0','1','Support Vector')
+% hold off
+
+% Visualize trainingSetPoints and edges
+% E = rgb2gray(I);
+% color = cat(3, zeros(size(E)), ones(size(E)), ones(size(E)));
+% imshow(I, 'initialMag', 100);
+% hold on;
+% h = imshow(color); 
+% hold off
+% set(h, 'AlphaData', trainingSetPoints);
+
+% hold on
+% color = cat(3, ones(size(E)), zeros(size(E)), ones(size(E)));
+% h1 = imshow(color); 
+% set(h1, 'AlphaData', edges);
+% edges = E(svm>=1);
+
+toc
--- src/trainSVM.m
+++ src/trainSVM.m
@@ -1,13 +1,21 @@
-function [svm, trainingFeatures, trainingLabels] = trainSVM(I, trainingPoints)
+function [svm, trainingFeatures, trainingLabels] = trainSVM(I1, I2, I3, trainingPoints1, trainingPoints2, trainingPoints3)
 % Trains an SVM based on an edges in an image and a training set of points 
 % I = the original image
 % trainingPoints = a binary array of ground truth points
 
-trainingFeatures = extractFeatures(I, trainingPoints);
+trainingFeatures1 = extractFeaturesHistBox(I1, trainingPoints1);
+trainingFeatures2 = extractFeaturesHistBox(I2, trainingPoints2);
+trainingFeatures3 = extractFeaturesHistBox(I3, trainingPoints3);
+trainingFeatures = [trainingFeatures1 ; trainingFeatures2; trainingFeatures3];
 trainingLabels = ones(size(trainingFeatures, 1), 1);
-nonFeatureEdges = findEdges(I) & ~trainingPoints;
-nonFeatures = extractFeatures(I, nonFeatureEdges);
-trainingFeatures = [trainingFeatures ; nonFeatures];
+nonFeatureEdges1 = findEdges(I1) & ~trainingPoints1;
+nonFeatureEdges2 = findEdges(I2) & ~trainingPoints2;
+nonFeatureEdges3 = findEdges(I3) & ~trainingPoints3;
+nonFeatures1 = extractFeaturesHistBox(I1, nonFeatureEdges1);
+nonFeatures2 = extractFeaturesHistBox(I2, nonFeatureEdges2);
+nonFeatures3 = extractFeaturesHistBox(I3, nonFeatureEdges3);
+nonFeatures = [nonFeatures1; nonFeatures2; nonFeatures3];
+trainingFeatures = [trainingFeatures; nonFeatures];
 trainingLabels = [trainingLabels ; zeros(size(nonFeatures,1), 1)];
 
 svm = fitcsvm(trainingFeatures(1:2:end,:), trainingLabels(1:2:end), ...
